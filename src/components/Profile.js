5810c8cc73344ac5e45101254a3669fdeb976ba7
Alice Lo <115280336+siuusunn@users.noreply.github.com>
2023-01-04 18:21:46 +0100

    Profile page now shows profile of logged in user

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,10 +3,20 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { API } from '../lib/api';
 import { AUTH } from '../lib/auth';
 
-export default function Profile({ id }) {
+import { Container } from '@mui/material';
+
+export default function Profile() {
   const navigate = useNavigate();
-  const [userData, setUserData] = useState(null);
-  const { userId } = useParams();
+  const { id } = useParams();
+  const [userData, setUserData] = useState({
+    username: '',
+    isAdmin: false,
+    email: '',
+    password: '',
+    cloudinaryImageId: ''
+  });
+
+  console.log(id);
 
   useEffect(() => {
     API.GET(API.ENDPOINTS.singleUser(id))
@@ -16,12 +26,17 @@ export default function Profile({ id }) {
       .catch(({ message, response }) => {
         console.error(message, response);
       });
-  }, []);
+  }, [id]);
+
+  console.log(userData);
 
   return (
     <>
-      <p>Profile component</p>
-      <p>{userData?.username}</p>
+      <Container>
+        <h1>User Profile</h1>
+        <p>Username: {userData?.username}</p>
+        <p>User ID: {userData?._id}</p>
+      </Container>
     </>
   );
 }
