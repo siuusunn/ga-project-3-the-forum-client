cb2452ba99960a0c1dee4db15576d829eb2e1773
Parul Singh <parul@paruls-air.home>
2023-01-04 20:55:16 +0000

    allposts layout done

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react';
-import { API } from '../lib/api';
+import { useEffect, useState } from "react";
+import { API } from "../lib/api";
+import { Grid, Paper, Box, styled } from "@mui/material";
 
 export default function PostsIndex() {
   const [posts, setPosts] = useState(null);
@@ -16,5 +17,25 @@ export default function PostsIndex() {
   }, []);
 
   console.log({ posts });
-  return <p>this is our discussion board</p>;
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "lightYellow",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+  return (
+    <Box sx={{ width: "40%" }}>
+      <Grid container spacing={4} columns={1} sx={{ marginLeft: "12px", marginTop:'20px' }}>
+      {posts?.map((post) => (
+          <Grid item xs={8} key={post._id} sx={{marginBottom:'5px'}}>
+           <Item>{post.text}</Item>
+          </Grid>
+          
+        ))}
+       
+      </Grid>
+    </Box>
+  );
 }
