466addd62c8aa3a2f25fa24a88391039af3be70a
Louis_Grant <ljsgrant@gmail.com>
2023-01-05 15:57:42 +0000

    reply form defocus after reply is submitted

--- src/components/common/CommentCard.js
+++ src/components/common/CommentCard.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { API } from '../../lib/api';
 import '../../styles/CommentCard.scss';
 
@@ -11,6 +11,7 @@ export default function CommentCard({
   commentId,
   setIsContentUpdated
 }) {
+  const formInput = useRef(null);
   const [newReplyFormFields, setNewReplyFormFields] = useState({
     text: ''
   });
@@ -21,14 +22,28 @@ export default function CommentCard({
 
   const handleReplySubmit = (event) => {
     event.preventDefault();
-    API.POST(API.ENDPOINTS.singleComment(commentId), newReplyFormFields, API.getHeaders())
+    API.POST(
+      API.ENDPOINTS.singleComment(commentId),
+      newReplyFormFields,
+      API.getHeaders()
+    )
       .then(({ data }) => {
         console.log(data);
         console.log('Posted comment');
         setIsContentUpdated(true);
       })
       .catch((err) => console.error(err));
+
+    setNewReplyFormFields({
+      text: ''
+    });
+
+    handleFocus();
   };
+
+  function handleFocus() {
+    formInput.current.blur();
+  }
 
   const handleDeleteComment = () => {
     API.DELETE(API.ENDPOINTS.singleComment(commentId), API.getHeaders())
@@ -52,18 +67,21 @@ export default function CommentCard({
           <p>
             Likes: {likes}, Dislikes: {dislikes}
           </p>
-          <button onClick={handleDeleteComment}>Delete</button>
-          <form onSubmit={handleReplySubmit}>
-            <label htmlFor='comment-text'> Reply: </label>
-            <input
-              type='text'
-              id='comment-text'
-              name='text'
-              value={newReplyFormFields.text}
-              onChange={handleNewReplyChange}
-            ></input>
-            <button type='submit'>Post reply</button>
-          </form>
+          <div className='comment-actions'>
+            <button onClick={handleDeleteComment}>Delete</button>
+            <form onSubmit={handleReplySubmit}>
+              <label htmlFor='comment-text'> Reply: </label>
+              <input
+                ref={formInput}
+                type='text'
+                id='comment-text'
+                name='text'
+                value={newReplyFormFields.text}
+                onChange={handleNewReplyChange}
+              ></input>
+              <button type='submit'>Post reply</button>
+            </form>
+          </div>
         </div>
         {comments?.map((comment) => {
           return (
