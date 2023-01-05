1f435218267fee548f7a913bf01259d091c8d921
Louis_Grant <ljsgrant@gmail.com>
2023-01-05 11:11:53 +0000

    can add comments to posts via basic UI

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -9,6 +9,9 @@ export default function SinglePost() {
   const navigate = useNavigate();
   const { id } = useParams();
   const [singlePost, setSinglePost] = useState(null);
+  const [newCommentFormFields, setNewCommentFormFields] = useState({
+    text: ''
+  });
 
   useEffect(() => {
     API.GET(API.ENDPOINTS.singlePost(id))
@@ -19,6 +22,24 @@ export default function SinglePost() {
         console.error(message, response);
       });
   }, [id]);
+
+  const handleNewCommentChange = (event) => {
+    setNewCommentFormFields({ [event.target.name]: event.target.value });
+  };
+
+  const handleNewCommentSubmit = (event) => {
+    event.preventDefault();
+
+    API.POST(
+      API.ENDPOINTS.commentsForPost(id),
+      newCommentFormFields,
+      API.getHeaders()
+    )
+      .then(({ data }) => {
+        console.log(data);
+      })
+      .catch((err) => console.error(err));
+  };
 
   return (
     <>
@@ -33,7 +54,20 @@ export default function SinglePost() {
             Likes: {singlePost?.likes} | Dislikes: {singlePost?.dislikes}
           </p>
         </Box>
-        <CommentThread comments={singlePost?.comments}/>
+        <form onSubmit={handleNewCommentSubmit}>
+          <div>
+            <label>Add a comment: </label>
+            <input
+              type='text'
+              id='text'
+              name='text'
+              value={newCommentFormFields.text}
+              onChange={handleNewCommentChange}
+            ></input>
+          </div>
+          <button type='submit'>Submit</button>
+        </form>
+        <CommentThread comments={singlePost?.comments} />
       </Container>
     </>
   );
