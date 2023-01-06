b89f503b62ec722ccb761d81ba3098b65d627795
Parul Singh <parul@paruls-air.home>
2023-01-06 16:25:06 +0000

    fix single post-single source of truth

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,98 +1,20 @@
-import { useState, useEffect } from 'react';
-import { useParams, useNavigate, Link } from 'react-router-dom';
-import { API } from '../lib/api';
-import CommentThread from './common/CommentThread';
-import { PostLikes } from './common/PostLikes';
+import { useState, useEffect } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
+import { API } from "../lib/api";
+import CommentThread from "./common/CommentThread";
+import { PostLikes } from "./common/PostLikes";
 
-import { Container, Box } from '@mui/material';
+import { Container, Box } from "@mui/material";
 
-import '../styles/SinglePost.scss';
+import "../styles/SinglePost.scss";
+import { DisplayPost } from "./DisplayPost";
 
 export const SinglePost = () => {
-  const [singlePost, setSinglePost] = useState(null);
-  const [newCommentFormFields, setNewCommentFormFields] = useState({
-    text: ''
-  });
-  const [isContentUpdated, setIsContentUpdated] = useState(false);
   const { id } = useParams();
-
-  useEffect(() => {
-    if (id) {
-      API.GET(API.ENDPOINTS.singlePost(id))
-        .then(({ data }) => {
-          setSinglePost(data);
-        })
-        .catch(({ message, response }) => {
-          console.error(message, response);
-        });
-    }
-    setIsContentUpdated(false);
-    console.log(isContentUpdated);
-  }, [id, isContentUpdated]);
-
-  const handleNewCommentChange = (event) => {
-    setNewCommentFormFields({ [event.target.name]: event.target.value });
-  };
-
-  const handleNewCommentSubmit = (event) => {
-    event.preventDefault();
-
-    API.POST(
-      API.ENDPOINTS.commentsForPost(id),
-      newCommentFormFields,
-      API.getHeaders()
-    )
-      .then(({ data }) => {
-        console.log(data);
-        setIsContentUpdated(true);
-      })
-      .catch((err) => console.error(err));
-  };
-
-  const humanDate = new Date(singlePost?.createdAt).toLocaleString();
 
   return (
     <>
-      <Container className='SinglePost'>
-        <Box>
-          <h1>{singlePost?.topic}</h1>
-          <p>
-            Posted by: {singlePost?.addedBy.username} on <i>{`${humanDate}`}</i>
-          </p>
-          <p>{singlePost?.content}</p>
-          <div className='likes-container-outer'>
-            <div className='likes-container-inner'>
-              <PostLikes
-                storedLikes={singlePost?.likes}
-                storedDislikes={singlePost?.dislikes}
-                id={id}
-                setIsContentUpdated={setIsContentUpdated}
-              />
-            </div>
-          </div>
-        </Box>
-        <div className='comments-container'>
-          <form onSubmit={handleNewCommentSubmit}>
-            <div>
-              <h3>Comments</h3>
-              <label htmlFor='comment-text'>Add a comment: </label>
-              <input
-                type='text'
-                id='comment-text'
-                name='text'
-                value={newCommentFormFields.text}
-                onChange={handleNewCommentChange}
-              ></input>
-            </div>
-            <button type='submit'>Submit</button>
-          </form>
-
-          <CommentThread
-            comments={singlePost?.comments}
-            setIsContentUpdated={setIsContentUpdated}
-          />
-        </div>
-      </Container>
+      <DisplayPost id={id}></DisplayPost>
     </>
   );
 };
