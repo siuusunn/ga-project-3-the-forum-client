f6685376ba98b95a36a040c7a0042659eb5c3d50
Louis_Grant <ljsgrant@gmail.com>
2023-01-07 18:22:31 +0000

    working on displaypost refresh when post deleted

--- src/components/DisplayPost.js
+++ src/components/DisplayPost.js
@@ -68,9 +68,6 @@ export const DisplayPost = ({ id, setPostsUpdated }) => {
         setPostsUpdated(true);
       })
       .catch((err) => console.error(err));
-
-    // setPostsUpdated(true);
-    // navigate('/posts');
   };
 
   const handleEditPost = () => {
@@ -96,94 +93,89 @@ export const DisplayPost = ({ id, setPostsUpdated }) => {
 
   const humanDate = new Date(singlePost?.createdAt).toLocaleString();
 
-  if (!singlePost) {
-    return <p>Post deleted</p>;
-  } else {
-    return (
-      <>
-        <Container className='SinglePost'>
-          <Box>
-            <h1>{singlePost?.topic}</h1>
-            <p>
-              Posted by: {singlePost?.addedBy.username} on{' '}
-              <i>{`${humanDate}`}</i>
-            </p>
-            <p>{singlePost?.content}</p>
-            <div className='likes-container-outer'>
-              <div className='likes-container-inner'>
-                <PostLikes
-                  storedLikes={singlePost?.likes}
-                  storedDislikes={singlePost?.dislikes}
-                  id={id}
-                  setIsContentUpdated={setIsContentUpdated}
-                />
-              </div>
+  return (
+    <>
+      <Container className='SinglePost'>
+        <Box>
+          <h1>{singlePost?.topic}</h1>
+          <p>
+            Posted by: {singlePost?.addedBy.username} on <i>{`${humanDate}`}</i>
+          </p>
+          <p>{singlePost?.content}</p>
+          <div className='likes-container-outer'>
+            <div className='likes-container-inner'>
+              <PostLikes
+                storedLikes={singlePost?.likes}
+                storedDislikes={singlePost?.dislikes}
+                id={id}
+                setIsContentUpdated={setIsContentUpdated}
+              />
             </div>
-            <div className='post-actions'>
-              <Button size='small' onClick={handleEditPost} variant='contained'>
-                Edit Post
-              </Button>
-              <Button
-                size='small'
-                color='error'
-                onClick={handleDeleteAlertOpen}
-                variant='contained'
-              >
-                Delete Post
-              </Button>
-            </div>
-          </Box>
-          <div className='comments-container'>
-            <form onSubmit={handleNewCommentSubmit}>
-              <div>
-                <h3>Comments</h3>
-                <label htmlFor='comment-text'>Add a comment: </label>
-                <input
-                  type='text'
-                  id='comment-text'
-                  name='text'
-                  value={newCommentFormFields.text}
-                  onChange={handleNewCommentChange}
-                ></input>
-              </div>
-              <button type='submit'>Submit</button>
-            </form>
-
-            <CommentThread
-              comments={singlePost?.comments}
-              setIsContentUpdated={setIsContentUpdated}
-            />
           </div>
-        </Container>
-        <Dialog
-          open={isDeleteAlertOpen}
-          onClose={handleDeleteAlertClose}
-          aria-labelledby='alert-dialog-title'
-          aria-describedby='alert-dialog-description'
-        >
-          <DialogTitle color='error' id='alert-dialog-title'>
-            {'Delete Post'}
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText id='alert-dialog-description'>
-              Are you sure you want to delete
-              {AUTH.isOwner(singlePost?.addedBy._id)
-                ? ' your'
-                : ` this user's`}{' '}
-              post <strong>{singlePost?.topic}</strong>? You can't undo this
-              action!
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleDeleteCancel} color='primary'>
-              Cancel
+          <div className='post-actions'>
+            <Button size='small' onClick={handleEditPost} variant='contained'>
+              Edit Post
             </Button>
-            <Button onClick={handleDeleteConfirm} color='error' autoFocus>
-              Delete
+            <Button
+              size='small'
+              color='error'
+              onClick={handleDeleteAlertOpen}
+              variant='contained'
+            >
+              Delete Post
             </Button>
-          </DialogActions>
-        </Dialog>
-      </>
-    );
-  }
+          </div>
+        </Box>
+        <div className='comments-container'>
+          <form onSubmit={handleNewCommentSubmit}>
+            <div>
+              <h3>Comments</h3>
+              <label htmlFor='comment-text'>Add a comment: </label>
+              <input
+                type='text'
+                id='comment-text'
+                name='text'
+                value={newCommentFormFields.text}
+                onChange={handleNewCommentChange}
+              ></input>
+            </div>
+            <button type='submit'>Submit</button>
+          </form>
+
+          <CommentThread
+            comments={singlePost?.comments}
+            setIsContentUpdated={setIsContentUpdated}
+          />
+        </div>
+      </Container>
+      <Dialog
+        open={isDeleteAlertOpen}
+        onClose={handleDeleteAlertClose}
+        aria-labelledby='alert-dialog-title'
+        aria-describedby='alert-dialog-description'
+      >
+        <DialogTitle color='error' id='alert-dialog-title'>
+          {'Delete Post'}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id='alert-dialog-description'>
+            Are you sure you want to delete
+            {AUTH.isOwner(singlePost?.addedBy._id)
+              ? ' your'
+              : ` this user's`}{' '}
+            post <strong>{singlePost?.topic}</strong>? You can't undo this
+            action!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDeleteCancel} color='primary'>
+            Cancel
+          </Button>
+          <Button onClick={handleDeleteConfirm} color='error' autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 };
