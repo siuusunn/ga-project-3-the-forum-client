dcb9bd4549c834f34c797e5158f95868b45a2be1
Alice Lo <115280336+siuusunn@users.noreply.github.com>
2023-01-07 21:16:29 +0100

    User can update profile with or without uploading a new profile picture

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,19 +4,28 @@ import { API } from '../lib/api';
 import { AUTH } from '../lib/auth';
 import { NOTIFY } from '../lib/notifications';
 
-import { Container, List, Button, Box, Paper, TextField } from '@mui/material';
+import {
+  Container,
+  List,
+  Button,
+  Box,
+  Paper,
+  TextField,
+  IconButton
+} from '@mui/material';
 
 import {
-  FileUploadOutlined,
   VisibilityOutlined,
-  VisibilityOffOutlined
+  VisibilityOffOutlined,
+  AccountCircleOutlined,
+  CalendarMonthOutlined,
+  PermIdentityOutlined
 } from '@mui/icons-material';
 
 import DisplaySinglePostOnProfile from '../components/common/DisplaySinglePostOnProfile';
 import ProfilePicture from './common/ProfilePicture';
 
 export default function Profile() {
-  const navigate = useNavigate();
   const { id } = useParams();
   const [userData, setUserData] = useState({
     username: '',
@@ -39,6 +48,14 @@ export default function Profile() {
   });
   const [file, setFile] = useState('');
 
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   const handleChange = (event) => {
     setFormFields({ ...formFields, [event.target.name]: event.target.value });
     console.log(formFields);
@@ -52,29 +69,48 @@ export default function Profile() {
   const handleSaveChanges = async (event) => {
     event.preventDefault();
 
-    const imageData = new FormData();
-    imageData.append('file', file);
-    imageData.append(
-      'upload_preset',
-      process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET
-    );
-
     try {
-      const cloudinaryResponse = await API.POST(
-        API.ENDPOINTS.cloudinary,
-        imageData
-      );
+      if (file !== '') {
+        const imageData = new FormData();
+        imageData.append('file', file);
+        imageData.append(
+          'upload_preset',
+          process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET
+        );
 
-      const imageId = cloudinaryResponse.data.public_id;
+        const cloudinaryResponse = await API.POST(
+          API.ENDPOINTS.cloudinary,
+          imageData
+        );
 
-      const apiReqBody = {
-        ...formFields,
-        cloudinaryImageId: imageId
-      };
+        const imageId = cloudinaryResponse.data.public_id;
+
+        const apiReqBody = {
+          ...formFields,
+          cloudinaryImageId: imageId
+        };
+
+        await API.PUT(
+          API.ENDPOINTS.singleUser(userData?._id),
+          apiReqBody,
+          API.getHeaders()
+        );
+
+        const loginData = await API.POST(API.ENDPOINTS.login, {
+          email: formFields.email,
+          password: formFields.password
+        });
+
+        AUTH.setToken(loginData.data.token);
+
+        NOTIFY.SUCCESS('User profile updated!');
+        setIsUpdated(true);
+        setIsEditMode(false);
+      }
 
       await API.PUT(
         API.ENDPOINTS.singleUser(userData?._id),
-        apiReqBody,
+        formFields,
         API.getHeaders()
       );
 
@@ -85,7 +121,6 @@ export default function Profile() {
 
       AUTH.setToken(loginData.data.token);
 
-      console.log('Logged in!');
       NOTIFY.SUCCESS('User profile updated!');
       setIsUpdated(true);
       setIsEditMode(false);
@@ -107,6 +142,8 @@ export default function Profile() {
 
   const userPosts = userData?.posts;
 
+  console.log(file);
+
   const humanDate = new Date(userData?.createdAt).toLocaleDateString();
 
   return (
@@ -120,11 +157,11 @@ export default function Profile() {
           mt: 3
         }}
       >
-        <h1>User Profile</h1>
+        <h1>{userData?.username}'s Profile</h1>
         <Paper
           elevation={6}
           sx={{
-            pt: 4,
+            pt: 5,
             pb: 4,
             pl: 10,
             pr: 10,
@@ -164,26 +201,70 @@ export default function Profile() {
                 onChange={handleChange}
                 required
               />
-              <TextField
-                fullWidth
-                id='password'
-                name='password'
-                label='New Password'
-                type='password'
-                variant='standard'
-                onChange={handleChange}
-                required
-              />
-              <TextField
-                fullWidth
-                id='passwordConfirmation'
-                name='passwordConfirmation'
-                label='New Password Confirmation'
-                type='password'
-                variant='standard'
-                onChange={handleChange}
-                required
-              />
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'flex-end',
+                  justifyContent: 'center',
+                  mb: 1,
+                  width: 400
+                }}
+              >
+                <TextField
+                  fullWidth
+                  id='password'
+                  name='password'
+                  label='New Password'
+                  type={showPassword ? 'text' : 'password'}
+                  variant='standard'
+                  onChange={handleChange}
+                  required
+                />
+                <IconButton
+                  aria-label='toggle-password-visibility'
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge='end'
+                >
+                  {showPassword ? (
+                    <VisibilityOffOutlined />
+                  ) : (
+                    <VisibilityOutlined />
+                  )}
+                </IconButton>
+              </Box>
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'flex-end',
+                  justifyContent: 'center',
+                  mb: 2,
+                  width: 400
+                }}
+              >
+                <TextField
+                  fullWidth
+                  id='passwordConfirmation'
+                  name='passwordConfirmation'
+                  label='New Password Confirmation'
+                  type={showPassword ? 'text' : 'password'}
+                  variant='standard'
+                  onChange={handleChange}
+                  required
+                />
+                <IconButton
+                  aria-label='toggle-password-visibility'
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge='end'
+                >
+                  {showPassword ? (
+                    <VisibilityOffOutlined />
+                  ) : (
+                    <VisibilityOutlined />
+                  )}
+                </IconButton>
+              </Box>
               <h4>Upload a new profile picture:</h4>
               <TextField
                 id='profile-picture'
@@ -192,6 +273,7 @@ export default function Profile() {
                 variant='standard'
                 onChange={handleFileChange}
                 required
+                sx={{ mb: 4 }}
               />
             </>
           ) : (
@@ -201,9 +283,18 @@ export default function Profile() {
                 imageWidth={200}
                 imageHeight={200}
               />
-              <p>Username: {userData?.username}</p>
-              <p>Joined on: {`${humanDate}`}</p>
-              <p>User ID: {userData?._id}</p>
+              <Box sx={{ display: 'flex', alignItems: 'center', mt: 3 }}>
+                <AccountCircleOutlined sx={{ mr: 1 }} />
+                {userData?.username}
+              </Box>
+              <Box sx={{ display: 'flex', alignItems: 'center', mt: 2 }}>
+                <CalendarMonthOutlined sx={{ mr: 1 }} />
+                {`${humanDate}`}
+              </Box>
+              <Box sx={{ display: 'flex', alignItems: 'center', mt: 2, mb: 5 }}>
+                <PermIdentityOutlined sx={{ mr: 1 }} />
+                {userData?._id}
+              </Box>
             </>
           )}
           {AUTH.isOwner(userData?._id) && (
@@ -216,7 +307,9 @@ export default function Profile() {
                   </Box>
                 </>
               ) : (
-                <Button onClick={toggleEditMode}>Edit Profile</Button>
+                <Button onClick={toggleEditMode}>
+                  Edit Profile & Password
+                </Button>
               )}
             </>
           )}
@@ -230,7 +323,7 @@ export default function Profile() {
             mb: 10
           }}
         >
-          <h3>Posts by user:</h3>
+          <h3>Posts by {userData?.username}</h3>
           <List>
             {userPosts?.length === 0 ? (
               <p>{userData?.username} has not posted anything yet</p>
