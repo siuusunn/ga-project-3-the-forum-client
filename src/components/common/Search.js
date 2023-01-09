4c4b00c227e3c01c86160017f4ac8e046ea0fde3
Alice Lo <115280336+siuusunn@users.noreply.github.com>
2023-01-09 12:06:07 +0100

    Clickable search results

--- src/components/common/Search.js
+++ src/components/common/Search.js
@@ -1,7 +1,7 @@
 import { TextField, Stack, Autocomplete, Box } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { API } from '../../lib/api';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { SearchOutlined } from '@mui/icons-material';
 
 export default function Search() {
@@ -44,7 +44,11 @@ export default function Search() {
         <Autocomplete
           id='free-solo-demo'
           freeSolo
-          options={posts?.map((option) => option.topic)}
+          getOptionLabel={(option) => option.topic}
+          options={posts?.map((option) => option)}
+          onChange={(event: any, option: any) => {
+            window.location.href = `/posts/${option._id}`;
+          }}
           renderInput={(params) => (
             <TextField
               {...params}
