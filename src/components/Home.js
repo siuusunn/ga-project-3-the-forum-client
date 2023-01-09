1669829bf523780f7a778ec180187ef127799f51
Alice Lo <115280336+siuusunn@users.noreply.github.com>
2023-01-09 13:09:08 +0100

    Revamped user list

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,36 @@
-import { Avatar, Box, Container, Paper, TextField } from '@mui/material';
-import { SearchRounded, TextFieldsOutlined } from '@mui/icons-material';
+import {
+  Avatar,
+  Box,
+  Container,
+  Paper,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  ListSubheader
+} from '@mui/material';
+
+import { thumbnail } from '@cloudinary/url-gen/actions/resize';
+import { byRadius } from '@cloudinary/url-gen/actions/roundCorners';
+import { focusOn } from '@cloudinary/url-gen/qualifiers/gravity';
+import { FocusOn } from '@cloudinary/url-gen/qualifiers/focusOn';
+
+import { AdvancedImage } from '@cloudinary/react';
 import { useEffect, useState } from 'react';
 import { API } from '../lib/api';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import ProfilePicture from './common/ProfilePicture';
-
 import adminPic from '../assets/adminPic.png';
+import { Cloudinary } from '@cloudinary/url-gen';
 
 export default function Home() {
   const [users, setUsers] = useState(null);
+
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME
+    }
+  });
 
   useEffect(() => {
     API.GET(API.ENDPOINTS.allUsers)
@@ -18,73 +39,87 @@ export default function Home() {
   }, []);
 
   return (
-    <Container
-      maxWidth='sm'
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        mt: 5
-      }}
-    >
-      <h1>The Forum </h1>
-      <Box
+    <>
+      <Container
+        maxWidth='sm'
         sx={{
           display: 'flex',
-          flexDirection: 'row',
+          flexDirection: 'column',
           alignItems: 'center',
-          mb: 4,
-          mt: 1
+          mt: 5
         }}
       >
-        <Avatar
-          src={adminPic}
-          alt='admin-profile-picture'
-          sx={{ width: 160, height: 160, mr: 4 }}
-        />
-        <div class='speech left'>
-          <p>
-            Hello, I am the much loved and respected administrator of{' '}
-            <b>The Forum</b>. Join us with these amazing users in interesting
-            and thought-provoking discussions!
-          </p>
-        </div>
-      </Box>
-      <Paper
-        sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          alignItems: 'center',
-          justifyContent: 'center',
-          pt: 6,
-          pb: 6
-        }}
-        elevation={6}
-      >
-        {users?.map((user) => (
-          <>
-            <Link to={`/profile/${user._id}`}>
-              <Box
-                sx={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                  ml: 2,
-                  mr: 2
-                }}
-              >
-                <ProfilePicture
-                  cloudinaryImageId={user.cloudinaryImageId}
-                  imageHeight={120}
-                  imageWidth={120}
-                />
-                <h5>{user.username}</h5>
-              </Box>
-            </Link>
-          </>
-        ))}
-      </Paper>
-    </Container>
+        <h1>The Forum </h1>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            mb: 4,
+            mt: 1
+          }}
+        >
+          <Avatar
+            src={adminPic}
+            alt='admin-profile-picture'
+            sx={{ width: 160, height: 160, mr: 4 }}
+          />
+          <div class='speech left'>
+            <p>
+              Hello, I am the much loved and respected administrator of{' '}
+              <b>The Forum</b>. Join us with these amazing users in interesting
+              and thought-provoking discussions!
+            </p>
+          </div>
+        </Box>
+        <Paper
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            alignItems: 'center',
+            justifyContent: 'center',
+            pt: 0,
+            pb: 0
+          }}
+          elevation={6}
+        >
+          <ImageList
+            sx={{
+              width: 500,
+              height: 370,
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'column',
+              flexWrap: 'wrap',
+              pl: 3,
+              pr: 3
+            }}
+          >
+            {users?.map((user) => (
+              <Link to={`/profile/${user._id}`}>
+                <ImageListItem
+                  key={user.username}
+                  sx={{
+                    width: 150,
+                    margin: 0,
+                    padding: 0
+                  }}
+                >
+                  {
+                    <AdvancedImage
+                      cldImg={cld
+                        .image(user.cloudinaryImageId)
+                        .resize(thumbnail().width(200).height(200))}
+                    />
+                  }
+                  <ImageListItemBar title={user.username} />
+                </ImageListItem>
+              </Link>
+            ))}
+          </ImageList>
+        </Paper>
+      </Container>
+    </>
   );
 }
